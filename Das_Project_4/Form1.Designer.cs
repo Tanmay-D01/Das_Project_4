924e0cb63e09bf36263ca39f92edc5f6369e8c52
TanmayD01 <tanmayd8616@gmail.com>
2025-09-10 11:54:51 -0400

    ICA 1 and ICA2 Finished

--- Das_Project_4/Form1.Designer.cs
+++ Das_Project_4/Form1.Designer.cs
@@ -138,7 +138,7 @@
             Controls.Add(txtnumwidgets);
             Margin = new Padding(2);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Tanmay-Das Form 1";
             Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
